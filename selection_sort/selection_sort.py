2b4202ceb0a5842920115d18c9b8797c96149da1
yaas-3r <167516893+yaas-3r@users.noreply.github.com>
2025-07-02 10:34:12 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,10 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+            arr[i], arr[min_idx] = arr[i]
+        return arr
+    

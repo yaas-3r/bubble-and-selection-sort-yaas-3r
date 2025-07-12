4377463e98e3e785f5a16279b8831ed22925ac52
Yaasir <yaasirmohammad397@gmail.com>
2025-07-12 12:04:34 +0300

    second commit for yaasir atiq

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,11 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    n = len(unsorted_list)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+                swapped = True
+        if not swapped:
+            break
+    return unsorted_list
